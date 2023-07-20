91889e0b66c56843e0961dfcd8fe8fd11be5b2e4
Robert.Hodder <robert.hodder@veterans.gc.ca>
2023-07-20 08:50:03 -0300

    Removed share button, try to align top text to the right

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -43,12 +43,12 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h3 className={styles.headerTitle}>Azure Open AI</h3>
+                            <h3 className={styles.headerTitleVAC}>Azure Open AI</h3>
                         </Link>
-                        <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
+                        {/* <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
                             <ShareRegular className={styles.shareButton} />
                             <span className={styles.shareButtonText}>Share</span>
-                        </div>
+                        </div> */}
                     </Stack>
                 </div>
             </header>
